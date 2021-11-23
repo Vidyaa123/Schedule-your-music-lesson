c255082035025ed03fb54a9167732573912cd974
Vidyaa <mailvidyaa@gmail.com>
2021-11-23 21:17:24 +0000

    Added about.js, layout.js and layout.module.css

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle ="About Me">
+            <h4>This is an app to schedule your music lessons at your conveneience.</h4>
+            <p>Choose your favourite teacher and fix your lessons based on their availability</p>
+        </Layout>
+        
+    )
+}
+
+export default AboutPage
